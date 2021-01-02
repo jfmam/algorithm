97e943a38e2d7bf66c3b63da3c0304a32fbf58cf
jfmam <jfmam@naver.com>
2021-01-02 12:23:30 +0900

    tues algorithm

--- seunghun/2주차/backjoon/나무자르기.py
+++ seunghun/2주차/backjoon/나무자르기.py
@@ -1,12 +1,12 @@
 def calcSum(ar, k):
-    arr = []
+    Sum = 0
     for i in ar:
         if i-k > 0:
-            arr.append(i-k)
-    return sum(arr)
+            Sum += (i-k)
+    return Sum
 
 
-def f(num, st, fin):
+def f(st, fin):
     global maxV
     global m
     global ar
@@ -14,13 +14,11 @@ def f(num, st, fin):
         return
     mid = (st + fin)//2
     Sum = calcSum(ar, mid)
-    if Sum > m:
-        return f(num, mid+1, fin)
-    elif Sum == m:
+    if Sum >= m:
         maxV = max(maxV, mid)
-        return f(num, mid+1, fin)
+        return f(mid+1, fin)
     else:
-        return f(num, st, mid-1)
+        return f(st, mid-1)
 
 
 maxV = 0
@@ -28,5 +26,5 @@ n, m = input().split(' ')
 n = int(n)
 m = int(m)
 ar = list(map(int, input().split(' ')))
-f(n, 0, max(ar))
+f(0, max(ar))
 print(maxV)
