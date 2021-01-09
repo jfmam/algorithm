81ff94f7f92bd0b71fe159b773787a71c889c069
jfmam <jfmam@naver.com>
2021-01-10 00:11:43 +0900

    write md

--- seunghun/2주차/backjoon/분산처리.py
+++ seunghun/2주차/backjoon/분산처리.py
@@ -1,22 +1,51 @@
-T = int(input())
+import sys
 
-for _ in range(T):
-    a, b = map(int, input().split())
-    a = a % 10
+t = int(input())
+arr = []
 
-    if a == 0:
-        print(10)
-    elif a == 1 or a == 5 or a == 6:
-        print(a)
-    elif a == 4 or a == 9:
-        b = b % 2
-        if b == 1:
-            print(a)
+for i in range(t):
+    n, m = input().split(' ')
+    arr.append((int(n), int(m)))
+
+
+
+for i in arr:
+    one = []
+    c = i[0]
+    for j in range(1, i[1]):
+        c *= j
+        if not c % 10 in one:
+            one.append(c)
         else:
-            print((a * a) % 10)
+            break
+    b = i[1] % len(one)
+    if b == 0: 
+        print(one[len(one) -1])
     else:
-        b = b % 4
-        if b == 0:
-            print((a**4) % 10 % 10 % 10)
-        else:
-            print((a**b) % 10 % 10 % 10)
+        print(one[b -1])
+
+#모범 답안
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    a, b = map(int, sys.stdin.readline())
+
+    #불 필요한 연산을 먼저 제거한다.
+    if a in [1,5,9]: #or연산자 대신 in 연산자 사용하기
+        print(a)
+        continue
+
+    result_list = []
+    temp = 1
+    for _ in range(b):
+        temp *= a
+        temp %= 10
+        if temp in result_list:
+            break
+        result_list.append(temp)
+    idx = b % len(result_list) -1
+    result = result_list[idx] # 1,5,9로 인해 정확한 idx를 구하는 것에 애를 먹었다.
+    if result == 0:
+        print(10)
+    else:
+        print(result) 
