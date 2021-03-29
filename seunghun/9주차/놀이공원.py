66a8774ff05ad86c3d0cf22ee9460c12416022e2
seung Lee <jfmam@naver.com>
2021-03-29 21:01:10 +0900

    sovle

--- seunghun/9주차/놀이공원.py
+++ seunghun/9주차/놀이공원.py
@@ -1,2 +1,26 @@
-for i in range(1,2):
-    print(i)
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+testCase = int(input())
+n, k = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+Min = 9999
+
+if k == 1:
+    print(1)
+    exit
+
+
+def check(i, j):
+    global Min
+    cnt = 0
+    for x in range(i, i+k-1):
+        for y in range(j, j+k-1):
+            if arr[x][y] == 1:
+                cnt += 1
+    Min = min(Min, cnt)
+
+
+for i in range(n-k):
+    for j in range(n-k):
+        check(i, j)
+print(Min)
