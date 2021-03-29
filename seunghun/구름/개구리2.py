083157271fe042878b92bece2cccf4cc55f27144
seung Lee <jfmam@naver.com>
2021-03-30 00:32:04 +0900

    fix unrequired code

--- seunghun/구름/개구리2.py
+++ seunghun/구름/개구리2.py
@@ -10,8 +10,6 @@ for i in range (len(arr)-1, 0, -1):
     answer = max(p, answer)
     answer += 1
 
+#왕눈이의 위치는 0 부터 시작임으로 -1 일 때도 계산이 필요하다.
 answer = max(arr[0], answer)
-
-if answer == p:
-    answer += 1
 print(answer)
