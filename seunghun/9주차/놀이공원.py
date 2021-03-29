5e12bfdd12fc0537c5ca8ba3f46a71e6a7e9b95e
jfmam <jfmam@naver.com>
2021-03-29 22:07:50 +0900

    solve ameusuem park

--- seunghun/9주차/놀이공원.py
+++ seunghun/9주차/놀이공원.py
@@ -1,26 +1,20 @@
 # -*- coding: utf-8 -*-
 # UTF-8 encoding when using korean
 testCase = int(input())
-n, k = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
-Min = 9999
 
-if k == 1:
-    print(1)
-    exit
+def execute(n, k, arr):
+    Min = 9999
+    for i in range(n-k+1):
+        for j in range(n-k+1):
+            cnt = 0
+            for x in range(i, i+k):
+                for y in range(j, j+k):
+                    if arr[x][y] == 1:
+                        cnt += 1
+            Min = min(Min, cnt)
+    print(Min)
 
-
-def check(i, j):
-    global Min
-    cnt = 0
-    for x in range(i, i+k-1):
-        for y in range(j, j+k-1):
-            if arr[x][y] == 1:
-                cnt += 1
-    Min = min(Min, cnt)
-
-
-for i in range(n-k):
-    for j in range(n-k):
-        check(i, j)
-print(Min)
+for _ in range(testCase):
+	n, k = map(int, input().split())
+	arr = [list(map(int, input().split())) for _ in range(n)]
+	execute(n, k, arr)
