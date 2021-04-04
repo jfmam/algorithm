2ced9c3776414086103eee3b7e6d4ac96ac9bbec
seung Lee <jfmam@naver.com>
2021-04-04 17:56:51 +0900

    forth tree

---
+++ seunghun/프로그래머스/쿼드압축 후 개수 세기.py
@@ -0,0 +1,31 @@
+ar = [[1, 1, 1, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1, 1, 1], [0, 0, 0, 0, 1, 1, 1, 1], [0, 1, 0, 0, 1, 1, 1, 1], [
+    0, 0, 0, 0, 0, 0, 1, 1], [0, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 1, 0, 0, 1], [0, 0, 0, 0, 1, 1, 1, 1]]
+answer = [0, 0]
+
+
+def DFS(arr, i, j, size):
+    base = True
+    pivot = arr[i][j]
+    for x in range(i, i+size):
+        for y in range(j, j+size):
+            if pivot != arr[x][y]:
+                base = False
+
+    if base:
+        answer[pivot] += 1
+    else:
+        newSize = size // 2
+        DFS(arr, i, j, newSize)
+        DFS(arr, i+newSize, j ,newSize)
+        DFS(arr, i, j+ newSize, newSize)
+        DFS(arr, i+ newSize, j+ newSize, newSize)
+
+
+def solution(arr):
+    arrLen = len(arr[0])
+    DFS(arr, 0, 0, arrLen)
+    return answer
+
+
+result = solution(ar)
+print(result)
