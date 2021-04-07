8b1d46ce0284b3f06f2220231c04327948527757
seung Lee <jfmam@naver.com>
2021-04-08 07:47:11 +0900

    phone number list

--- seunghun/프로그래머스/완주하지 못한 선수.py
+++ seunghun/프로그래머스/완주하지 못한 선수.py
@@ -8,3 +8,10 @@ def solution(participant, completion):
             break
 
     return answer
+
+# array 다루기
+# arr.index("찾으려는 아이템")
+# arr.remove("제거하려는 아이템")
+# del arr[arr.index('item')] 은 remove와 같은 효과
+
+# 효율성을 위해서는 1중 for문으로 처리 또한 answer를 찾을 시 break 필요
