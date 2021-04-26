45f52adf84fb152b51f0937ecd5448e3c404f33c
seung Lee <jfmam@naver.com>
2021-04-27 00:40:51 +0900

    fix error

--- seunghun/프로그래머스/섬 연결하기.py
+++ seunghun/프로그래머스/섬 연결하기.py
@@ -5,15 +5,15 @@ sys.setrecursionlimit(10 ** 9)
 def solution(n, costs):
     ans = 0
     costs.sort(key= lambda x: x[2] ) # cost 기준으로 정렬
-    routes = set([costs[0][0]])
-    while len(routes) != n :
+    cycle = set([costs[0][0]]) # cycle의 집합
+    while len(cycle) != n :
         for i, cost in enumerate(costs):
-            if cost[0] in routes and cost[1] in routes:
+            if cost[0] in cycle and cost[1] in cycle:# cycle에 속해 있으면 해당 값 반환
                 continue
-            if cost[0] in routes or cost[i] in routes:
-                routes.update([cost[0], cost[1]])
+            if cost[0] in cycle or cost[1] in cycle:
+                cycle.update([cost[0], cost[1]])
                 ans += cost[2]
-                costs[i] = [-1, -1, -1]
+                del(costs[i])
                 break
     return ans
 
