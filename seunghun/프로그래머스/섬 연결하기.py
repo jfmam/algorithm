12033851e4a2344dc91ba802373abe0cdb1df67d
seung Lee <jfmam@naver.com>
2021-04-26 23:40:26 +0900

    connect island

--- seunghun/프로그래머스/섬 연결하기.py
+++ seunghun/프로그래머스/섬 연결하기.py
@@ -3,33 +3,19 @@ sys.setrecursionlimit(10 ** 9)
 
 
 def solution(n, costs):
-    global Min
-    visited = [0] * n
-    arr = [[] for _ in range(n)]
-    Min = 9999
-
-    for i in costs:
-        arr[i[0]].append({'a': i[1], 'cost': i[2]})
-        arr[i[1]].append({'a': i[0], 'cost': i[2]})
-
-    def DFS(node, idx, cnt):
-        global Min
-        if idx == n:
-            Min = min(Min, cnt)
-            return;
-        else:
-            visited[node] = 1
-            for i in arr[node]:
-                if visited[i['a']] == 0:
-                    visited[i['a']] = 1
-                    DFS(i['a'], idx + 1, cnt + i['cost'])
-                    visited[i['a']] = 0
-                    
-    for i in range(n):
-        visited =[0] * n
-        DFS(i, 1, 0)
-    return Min
-
+    ans = 0
+    costs.sort(key= lambda x: x[2] ) # cost 기준으로 정렬
+    routes = set([costs[0][0]])
+    while len(routes) != n :
+        for i, cost in enumerate(costs):
+            if cost[0] in routes and cost[1] in routes:
+                continue
+            if cost[0] in routes or cost[i] in routes:
+                routes.update([cost[0], cost[1]])
+                ans += cost[2]
+                costs[i] = [-1, -1, -1]
+                break
+    return ans
 
 ans = solution(4, [[0, 1, 1], [0, 2, 2], [1, 2, 5], [1, 3, 1], [2, 3, 8]])
 print(ans)
